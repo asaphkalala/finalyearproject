776894783b008c45ed9e7d4c16a40ccc604f16e7
as☆ph <83978332+asaphkalala@users.noreply.github.com>
2023-06-14 22:57:21 +0100

    Delete afrohairopengl directory

--- afrohairopengl/out/build/x64-Debug/src/PathConfig.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#define TEXTURE_FOLDER std::string("C:/Users/asaph/Downloads/FinalYear/afrohairopengl/Textures/")
-#define SHADER_FOLDER std::string("C:/Users/asaph/Downloads/FinalYear/afrohairopengl/src/Shaders/")
